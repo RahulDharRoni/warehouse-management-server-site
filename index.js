46f80ca6e63360b0498feea41bbf26442b346806
RahulDharRoni <freelancerrahulroni@gmail.com>
2022-05-03 04:08:44 +0600

    all get update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express();
 var cors = require('cors')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express());
+app.use(bodyParser());
+
 
 
 const uri = "mongodb+srv://books:D8Kn1IyvFUNTbnHN@cluster0.un1it.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -14,27 +16,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const serviceCollection = client.db("booksCollection").collection("collection");
+        const booksCollection = client.db("booksCollection").collection("collection");
         app.get('/books', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = booksCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
         app.get('/books/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
+            const service = await booksCollection.findOne(query);
             res.send(service);
         })
-
         app.post('/books', async (req, res) => {
             const newService = req.body;
-            const service = await serviceCollection.insertOne(newService);
-            res.send(service);
-        })
+            const result = await booksCollection.insertOne(newService);
+            res.send(result);
+        });
+        // app.post('/books', async (req, res) => {
+        //     const newBook = req.body;
+        //     console.log(newBook);
+        //     const books = await booksCollection.insertOne(newBook);
+        //     res.send(books)
+        // })
     } finally {
-        // await client.close();
+
     }
 }
 run().catch(console.dir);
